62fafc4ef26db67d44a80b3c245be2c9dbcb1406
Michał Ryczańczyk <michal@ryczanczyk.net>
2021-02-28 23:47:02 +0100

    Supplier2

---
+++ src/Supplier/Supplier2.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Supplier;
+
+use App\Exception\BadResponseException;
+use App\Exception\InvalidParserException;
+
+class Supplier2  extends SupplierAbstract
+{
+    public static function getName(): string
+    {
+        return 'supplier2';
+    }
+    public static function getResponseType(): string
+    {
+        return 'xml';
+    }
+
+    /**
+     * @return mixed[]
+     * @throws InvalidParserException
+     */
+    protected function parseResponse(): array
+    {
+        $response = $this->getResponse();
+        if (!is_string($response)) {
+            throw new BadResponseException(self::getName());
+        }
+        $parsedResponse = $this->parser->parse($response);
+
+        return $parsedResponse['item'];
+    }
+
+    protected function getResponse(): string|bool
+    {
+        return file_get_contents('http://nginx/suppliers/supplier2.xml');
+    }
+}
